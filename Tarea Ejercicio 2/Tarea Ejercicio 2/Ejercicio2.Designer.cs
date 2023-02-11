da1f64684e5cbe535767d0e0c9e227d0ce7d4e61
emilcemidence <emmc0798@gmail.com>
2023-02-10 19:16:22 -0600

    Segundo commit
    Segundo commit del ejercicio

--- Tarea Ejercicio 2/Tarea Ejercicio 2/Ejercicio2.Designer.cs
+++ Tarea Ejercicio 2/Tarea Ejercicio 2/Ejercicio2.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Tarea_Ejercicio_2
 {
-    partial class Form1
+    partial class Ejercicio2
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -29,13 +29,193 @@ namespace Tarea_Ejercicio_2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.IngresartextBox1 = new System.Windows.Forms.TextBox();
+            this.IngresartextBox2 = new System.Windows.Forms.TextBox();
+            this.IngresartextBox3 = new System.Windows.Forms.TextBox();
+            this.IngresartextBox4 = new System.Windows.Forms.TextBox();
+            this.Calcularbutton1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.PromediotextBox1 = new System.Windows.Forms.TextBox();
+            this.Limpiarbutton1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.CondiciontextBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(166, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(308, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calcular el promedio final de un estudiante";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingresar nota 1:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(115, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ingresar nota 2:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(115, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ingresar nota 3:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(115, 246);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ingresar nota 4:";
+            // 
+            // IngresartextBox1
+            // 
+            this.IngresartextBox1.Location = new System.Drawing.Point(233, 119);
+            this.IngresartextBox1.Name = "IngresartextBox1";
+            this.IngresartextBox1.Size = new System.Drawing.Size(100, 20);
+            this.IngresartextBox1.TabIndex = 5;
+            // 
+            // IngresartextBox2
+            // 
+            this.IngresartextBox2.Location = new System.Drawing.Point(233, 158);
+            this.IngresartextBox2.Name = "IngresartextBox2";
+            this.IngresartextBox2.Size = new System.Drawing.Size(100, 20);
+            this.IngresartextBox2.TabIndex = 6;
+            // 
+            // IngresartextBox3
+            // 
+            this.IngresartextBox3.Location = new System.Drawing.Point(233, 199);
+            this.IngresartextBox3.Name = "IngresartextBox3";
+            this.IngresartextBox3.Size = new System.Drawing.Size(100, 20);
+            this.IngresartextBox3.TabIndex = 7;
+            // 
+            // IngresartextBox4
+            // 
+            this.IngresartextBox4.Location = new System.Drawing.Point(233, 239);
+            this.IngresartextBox4.Name = "IngresartextBox4";
+            this.IngresartextBox4.Size = new System.Drawing.Size(100, 20);
+            this.IngresartextBox4.TabIndex = 8;
+            // 
+            // Calcularbutton1
+            // 
+            this.Calcularbutton1.Location = new System.Drawing.Point(258, 300);
+            this.Calcularbutton1.Name = "Calcularbutton1";
+            this.Calcularbutton1.Size = new System.Drawing.Size(75, 23);
+            this.Calcularbutton1.TabIndex = 9;
+            this.Calcularbutton1.Text = "Calcular";
+            this.Calcularbutton1.UseVisualStyleBackColor = true;
+            this.Calcularbutton1.Click += new System.EventHandler(this.Calcularbutton1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(167, 359);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Promedio:";
+            // 
+            // PromediotextBox1
+            // 
+            this.PromediotextBox1.Location = new System.Drawing.Point(245, 352);
+            this.PromediotextBox1.Name = "PromediotextBox1";
+            this.PromediotextBox1.Size = new System.Drawing.Size(100, 20);
+            this.PromediotextBox1.TabIndex = 11;
+            // 
+            // Limpiarbutton1
+            // 
+            this.Limpiarbutton1.Location = new System.Drawing.Point(384, 300);
+            this.Limpiarbutton1.Name = "Limpiarbutton1";
+            this.Limpiarbutton1.Size = new System.Drawing.Size(75, 23);
+            this.Limpiarbutton1.TabIndex = 12;
+            this.Limpiarbutton1.Text = "Limpiar";
+            this.Limpiarbutton1.UseVisualStyleBackColor = true;
+            this.Limpiarbutton1.Click += new System.EventHandler(this.Limpiarbutton1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(164, 408);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Condicion:";
+            // 
+            // CondiciontextBox1
+            // 
+            this.CondiciontextBox1.Location = new System.Drawing.Point(245, 408);
+            this.CondiciontextBox1.Name = "CondiciontextBox1";
+            this.CondiciontextBox1.Size = new System.Drawing.Size(100, 20);
+            this.CondiciontextBox1.TabIndex = 14;
+            // 
+            // Ejercicio2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CondiciontextBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Limpiarbutton1);
+            this.Controls.Add(this.PromediotextBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Calcularbutton1);
+            this.Controls.Add(this.IngresartextBox4);
+            this.Controls.Add(this.IngresartextBox3);
+            this.Controls.Add(this.IngresartextBox2);
+            this.Controls.Add(this.IngresartextBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Ejercicio2";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox IngresartextBox1;
+        private System.Windows.Forms.TextBox IngresartextBox2;
+        private System.Windows.Forms.TextBox IngresartextBox3;
+        private System.Windows.Forms.TextBox IngresartextBox4;
+        private System.Windows.Forms.Button Calcularbutton1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox PromediotextBox1;
+        private System.Windows.Forms.Button Limpiarbutton1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox CondiciontextBox1;
     }
 }
 
--- Tarea Ejercicio 2/Tarea Ejercicio 2/Ejercicio2.cs
+++ Tarea Ejercicio 2/Tarea Ejercicio 2/Ejercicio2.cs
@@ -10,11 +10,48 @@ using System.Windows.Forms;
 
 namespace Tarea_Ejercicio_2
 {
-    public partial class Form1 : Form
+    public partial class Ejercicio2 : Form
     {
-        public Form1()
+        public Ejercicio2()
         {
             InitializeComponent();
+        }
+
+        private void Calcularbutton1_Click(object sender, EventArgs e)
+        {
+            double nota1, nota2, nota3, nota4, promedio;
+            string condicion;
+
+            nota1 = double.Parse(IngresartextBox1.Text);
+            nota2 = double.Parse(IngresartextBox2.Text);
+            nota3 = double.Parse(IngresartextBox3.Text);
+            nota4 = double.Parse(IngresartextBox4.Text);
+
+            promedio = (nota1 + nota2 + nota3 + nota4) / 4;
+
+            if (promedio <= 64)
+            {
+                condicion = "DESAPROBADO";
+                PromediotextBox1.Text = promedio.ToString();
+                CondiciontextBox1.Text = condicion;
+            }
+            else if (promedio > 65 && promedio <= 100)
+            {
+                condicion = "APROBADO";
+                PromediotextBox1.Text = promedio.ToString();
+                CondiciontextBox1.Text = condicion;
+            }
+
+        }
+
+        private void Limpiarbutton1_Click(object sender, EventArgs e)
+        {
+            IngresartextBox1.Clear();
+            IngresartextBox2.Clear();
+            IngresartextBox3.Clear();
+            IngresartextBox4.Clear();
+            CondiciontextBox1.Clear();
+            IngresartextBox1.Focus();
         }
     }
 }
